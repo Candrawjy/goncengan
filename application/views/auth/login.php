acbc381cb3d72beafd1f9f1b832f3711013f5ea0
Candrawjy <candraw71@gmail.com>
2022-11-14 02:56:23 +0700

    removed google logo on auth

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -37,7 +37,7 @@
             <div class="mb-0 d-grid">
               <button type="submit" class="btn btn-warning btn-ecomm rounded-3">Masuk</button>
             </div>
-            <div class="separator my-4">
+            <!-- <div class="separator my-4">
               <div class="line"></div>
               <p class="mb-0 fw-bold px-3">Atau</p>
               <div class="line"></div>
@@ -46,7 +46,7 @@
               <a href="javascript:;" class="bg-pinterest">
                 <i class="bi bi-google"></i>
               </a>
-            </div>
+            </div> -->
           </form>
         </div>
       </div>
